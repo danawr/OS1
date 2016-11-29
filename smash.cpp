d98fe46f75bbf8abf975e289d83a2120578f7620
ZorinIvan <ivanzorin.mail@gmail.com>
2016-11-30 01:20:21 +0200

    merged 2.0

--- smash.cpp
+++ smash.cpp
@@ -11,31 +11,20 @@ main file. This file contains the main function of smash
 #include <signal.h>
 #include "commands.h"
 #include "signals.h"
-<<<<<<< HEAD
-#include "job.h"
-
-=======
 #include "Job.h"
->>>>>>> c098822aaef490a2449c5a8f36496536370f0fa4
 #define MAX_LINE_SIZE 80
 #define MAXARGS 20
 #define MAX_CMD_HISTORY_SIZE 50
 char* L_Fg_Cmd;
-<<<<<<< HEAD
 int SMASH_PID;
 std::vector<job> jobs; //This represents the list of jobs. Please change to a preferred type (e.g array of char*)
-=======
-
->>>>>>> c098822aaef490a2449c5a8f36496536370f0fa4
 char lineSize[MAX_LINE_SIZE];
 extern std::deque<std::string> cmd_history;
 
-<<<<<<< HEAD
-=======
 //extern std::deque<std::string> cmd_history;
 
 #define MAX_CMD_HISTORY_SIZE 50
->>>>>>> c098822aaef490a2449c5a8f36496536370f0fa4
+
 
 //**************************************************************************************
 // function name: main
@@ -48,31 +37,15 @@ int main(int argc, char *argv[])
 
 
 	//signal declaretions
-<<<<<<< HEAD
 	sighandler(SIGTSTP, &ctrl_z);
 	sighandler(SIGINT, &ctrl_c);
-=======
-	//NOTE: the signal handlers and the function/s that sets the handler should be found in siganls.c
-	 /* add your code here */
-
-	/************************************/
-	//NOTE: the signal handlers and the function/s that sets the handler should be found in siganls.c
-	//set your signal handlers here
-	/* add your code here */
-
-	/************************************/
->>>>>>> c098822aaef490a2449c5a8f36496536370f0fa4
 
 	/************************************/
 	// Init globals
 
-<<<<<<< HEAD
 	SMASH_PID = getpid();
     //jobs = NULL;
-=======
 
-
->>>>>>> c098822aaef490a2449c5a8f36496536370f0fa4
 	L_Fg_Cmd =(char*)malloc(sizeof(char)*(MAX_LINE_SIZE+1));
 	if (L_Fg_Cmd == NULL)
 			exit (-1);
